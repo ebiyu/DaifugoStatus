d4fbf82e5c734853a915c56ae27bbb61a3f59e0e
ebiyuu1121 <ebiyuu1121@gmail.com>
2017-06-24 11:44:27 +0900

    Initial Commit

---
+++ Daifugo StatusTests/Daifugo_StatusTests.swift
@@ -0,0 +1,36 @@
+//
+//  Daifugo_StatusTests.swift
+//  Daifugo StatusTests
+//
+//  Created by 海老原祐輔 on 2017/06/24.
+//  Copyright © 2017年 海老原祐輔. All rights reserved.
+//
+
+import XCTest
+@testable import Daifugo_Status
+
+class Daifugo_StatusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
